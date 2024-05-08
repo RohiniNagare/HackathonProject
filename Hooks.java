fd8a9892191b7517502fca7d2cfc2300989e22c5
Rohini Dnyaneshwar Nagare <99395794+RohiniNagare@users.noreply.github.com>
2024-05-08 15:30:35 +0530

    Add files via upload

---
+++ Hooks.java
@@ -0,0 +1,47 @@
+package stepDefinition;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import testBase.BaseClass;
+
+public class Hooks {
+
+	public static WebDriver driver=BaseClass.driver;
+     
+	//@Before//calling in step 1 @Given
+//    public void setup() throws IOException
+//    {
+//	 BaseClass bclass = new BaseClass();
+//	 bclass.driverSetup("chrome");			
+//	}
+	
+    @AfterAll
+    public static void After_All()
+    {
+    	driver.quit();
+    }
+    
+    
+    @AfterStep
+    public void addScreenshot(Scenario scenario) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+        boolean b = scenario.isFailed();
+        
+        if (!b) {
+        	// Attach the screenshot for passed steps
+            scenario.attach(screenshot, "image/png", "PassedStepScreenshot");
+        } else {
+            
+        	// Attach the screenshot for failed steps
+            scenario.attach(screenshot, "image/png", "FailedStepScreenshot");
+           
+        }
+    }
+
+   
+}
